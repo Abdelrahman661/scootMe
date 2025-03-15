9d5d36b470ae9a0540552777ce3bf448862fdaaf
Abdelrahman661 <152468218+Abdelrahman661@users.noreply.github.com>
2025-03-15 22:46:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet, Link } from "react-router-dom";
 import "./App.css";
-
+import {welcomelogo} from "/public/welcomelogo.svg"
 function App() {
   return (
     <div className="flex flex-col h-screen w-full bg-white">
       <div className="flex flex-col items-center justify-center flex-grow">
         <div className="mb-4">
-          <img src="../public/welcomelogo.svg" alt="" />
+          <img src={welcomelogo} alt="" />
         </div>
 
         <div className="w-60 h-60">
