99cb818da575de038edaf08d0558244c9b37fd60
Nabil Alex <nabilmohaed46@gmail.com>
2025-04-22 00:25:06 +0200

    add animation for bookRide

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { MdElectricScooter } from "react-icons/md";
+import { motion } from "framer-motion";
 import "./App.css";
-import welcomelogo from "../public/welcomelogo.svg"
-import welcomeland from "../public/welcomeland.svg"
+import welcomelogo from "../public/welcomelogo.svg";
+import welcomeland from "../public/welcomeland.svg";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen w-full bg-white">
+    <div className="relative flex flex-col h-screen w-full bg-white">
       <div className="flex flex-col items-center justify-center flex-grow">
         <div className="mb-4">
-          <img src={welcomelogo} alt="" />
+          <img src={welcomelogo} alt="Logo" />
         </div>
-
         <div className="w-60 h-60">
           <img
             src={welcomeland}
@@ -28,6 +30,18 @@ function App() {
           Get Started
         </button>
       </Link>
+
+      <Link to="/book-ride" className="absolute bottom-24 right-6">
+        <motion.div
+          animate={{ y: [0, -15, 0] }}
+          transition={{ repeat: Infinity, duration: 1.5 }}
+          className="p-2 bg-white rounded-full shadow-lg"
+        >
+          <MdElectricScooter className="text-4xl text-orange-500 hover:scale-110 transition-transform" />
+        </motion.div>
+      </Link>
+
+      <Outlet />
     </div>
   );
 }
